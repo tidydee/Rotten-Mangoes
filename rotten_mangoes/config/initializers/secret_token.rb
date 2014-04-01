09ef6992dccd9231c7af9c4ccde9949eb8ae67a3
dawidspamer <dawidspamer@gmail.com>
2014-04-01 02:01:32 +0000

    adding new Rails project and generating the first model

---
+++ rotten_mangoes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '8a278193256150d5d6dafe1f2241f55be6ae8adb2a6bbfdb369245d1b1c2bb5778c71f268101eafb7e256600d7e46eba398c4893aed6ae14ddb2727bfdffed6a'
---
+++ rotten_mangoes/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RottenMangoes::Application.config.session_store :cookie_store, key: '_rotten_mangoes_session'
